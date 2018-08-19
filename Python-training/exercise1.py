59352ee5e01a9e777c645123e8c208da9dc13be3
Sangjin Lee <isangjin77@gmail.com>
2018-08-19 14:05:55 +0000

    first py commit
    Signed-off-by: Sangjin Lee <isangjin77@gmail.com>

---
+++ Python-training/exercise1.py
@@ -0,0 +1,119 @@
+
+
+print 0b10
+print 0o10
+print 0x10
+
+print int(True)
+print int(False)
+
+print int(98.6)
+print int(1.0e4)
+print int(99)
+print int(-23)
+print int(+34)
+print int('+64')
+
+print 5+3.5
+print True+2
+print False+4
+
+print float(True)
+#print double(True)
+
+#print str(/n)
+
+print bool(True)
+
+print str(98.6)
+print str(1.0e4)
+
+
+palindrome = '\nA man, \nA plam, \nA canal: \nPanama\n'
+print(palindrome)
+
+print('\tabc')
+print('a\tbc')
+print('ab\tc')
+print('abc\t')
+
+testimony = "\"I did nothing!\" he said. \"Not that either! Or the other thing.\""
+fact = "The world's largest rubber duck was 54'2\" by 65'7\" by 105'"
+speech = 'Today we honor our friend, the backslash: \\.'
+
+print(testimony)
+print(fact)
+print(speech)
+
+print('\nRelease the kraken!' + 'At once!')
+print("My word! " "A gentleman caller!")
+
+start = 'Na' * 4 + '\n'
+middle = 'Hey' *3 + '\n'
+end = 'Goodbye.'
+
+print('\n')
+print(start + start + middle + end)
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+print letters[0]
+print letters[1]
+print letters[-1]
+print letters[-2]
+print (letters[25])
+print (letters[5])
+#print letters[100]
+
+name = 'Henny'
+print name
+print '\n'
+
+#name[0] = 'P'
+name.replace('H', 'P')
+print name
+print name.replace('H', 'P')
+
+print'\n'
+'C' + name[1:]
+print name
+print 'C' + name[1:]
+print 'a' + name[2:]
+
+print letters[:]
+print letters[20:]
+print letters[10:]
+print letters[12:15]
+print letters[-3:]
+print letters[18:-3]
+print letters[-6:-2]
+print '\n'
+
+print letters[::2]
+print letters[::7]
+print letters[4:20:3]
+print letters[19::4]
+print letters[:21:5]
+print'\n'
+
+print letters[::-1]
+print letters[-50:]
+print letters[-51:-50]
+print letters[:70]
+print letters[70:71]
+print letters[1:25]
+print letters[0:4]
+
+
+
+
+'''
+First Python comment
+
+luminus7
+first taste of py
+
+What for?: Connect website to search for videos info
+
+HTML -> JSON(Javascript Object Notation)
+
+'''
